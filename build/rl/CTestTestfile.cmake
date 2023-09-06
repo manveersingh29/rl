0578181dd874e90511ca8809e96ba5bca0f43fc7
manveersingh29 <91839032+manveersingh29@users.noreply.github.com>
2023-09-07 01:40:23 +0530

    Delete build directory

--- build/rl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/manveer/research_ws/src/rl
-# Build directory: /home/manveer/research_ws/build/rl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
